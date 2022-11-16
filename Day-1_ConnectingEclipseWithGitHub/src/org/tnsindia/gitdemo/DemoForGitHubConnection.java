4099f6c871d9e8f766d8c835704183403b758f56
Rajesh Jadhav <Rajesh Jadhav@DESKTOP-7OBCH5T>
2022-11-16 20:36:29 +0530

    first commit

---
+++ Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,10 @@
+package org.tnsindia.gitdemo;
+// Demo on to connect eclipse with GitHub
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome Home");
+
+	}
+
+}
